6867cd345aa682a17859b5070895dae6017fea1d
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-05 21:28:12 +0000

    added get_total_time to sql client

--- src/sql_client.py
+++ src/sql_client.py
@@ -1,5 +1,7 @@
-import os
+import datetime as dt
 from sqlalchemy import create_engine
+
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 class SQLClient():
@@ -8,9 +10,34 @@ class SQLClient():
         self,
         db_file: str,
     ):
-        self.engine = create_engine(f"sqlite:////{db_file}")
-        self.engine.connect()
+        self.engine = create_engine(f"sqlite:///{db_file}")
+
+    def get_total_time(
+        self,
+        task_name: str,
+    ) -> float:
+        """get the total number of hours spent on task_name"""
+
+        total_duration = 0
+
+        with self.engine.connect() as conn:
+            results = conn.exec_driver_sql((
+                "SELECT start_time, end_time "
+                "FROM work_hours "
+                f"WHERE issue = '{task_name}'"
+            ))
+            for row in results:
+                start_time = dt.datetime.strptime(row[0], DATETIME_FORMAT)
+                end_time = dt.datetime.strptime(row[1], DATETIME_FORMAT)
+
+                duration = (end_time - start_time).total_seconds()
+                total_duration += duration
+
+        return total_duration/3600
 
 
 if __name__ == "__main__":
-    client = SQLClient("brother.db")
+    client = SQLClient("/home/atschirhard/Sources/lazyjira/brother.db")
+    print(client.get_total_time("GTMP-1") == 16.0)
+    print(client.has_project("GTMP-1"))
+    input("end")
