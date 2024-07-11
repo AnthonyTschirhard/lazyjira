14bf5041a17b5753008d3deb07cce37902462ff4
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-11 22:46:55 +0000

    added 2 child tasks objects with different constructors

--- src/task.py
+++ src/task.py
@@ -1,8 +1,9 @@
 import datetime as dt
+from jira.resources import Issue as JiraIssue
 
 
-class Task():
-    """a Task Object"""
+class BaseTask():
+    """a Base Task Object"""
 
     def __init__(
         self,
@@ -10,6 +11,7 @@ class Task():
         name: str,
         project: str,
         status: str,
+        jira_id: str,
         description: str,
         complexity: int,
         parent: int,
@@ -33,6 +35,7 @@ class Task():
         self.name = name
         self.project = project
         self.status = status
+        self.jira_id = jira_id
         self.description = description
         self.complexity = complexity
         self.parent = parent
@@ -40,6 +43,51 @@ class Task():
         self.updated_date = updated_date
         self.due_date = due_date
         self.priority = priority
+
+
+class DBTask(BaseTask):
+    """a task object created from local databse"""
+
+    def __init__(
+        self,
+    ):
+        super().__init__()
+
+
+class JiraTask(BaseTask):
+    """a task object created from a jira task"""
+
+    def __init__(
+        self,
+        issue: JiraIssue,
+    ):
+
+        jira_id = issue.get_field("key")
+        name = issue.get_field("name")
+        status = issue.get_field("status")
+        # project =
+        # description =
+        # complexity =
+        # parent =
+        # created_date =
+        # updated_date =
+        # due_date =
+        # priority =
+
+        super().__init__(
+            id=None,
+            name=name,
+            project=None,
+            status=status,
+            jira_id=jira_id,
+            description=None,
+            complexity=None,
+            parent=None,
+            created_date=None,
+            updated_date=None,
+            due_date=None,
+            priority=None,
+        )
 
 
 if __name__ == "__main__":
