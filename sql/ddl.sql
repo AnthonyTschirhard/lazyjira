35bd5d293b9b04936090e00c24ed23735883565c
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-11 22:02:57 +0000

    reorg primary keys and add fields in tasks

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,26 +1,36 @@
-CREATE TABLE task (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    project TEXT
-);
-
 CREATE TABLE project (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
 
-CREATE TABLE work_hour (
+CREATE TABLE task (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    project TEXT NOT NULL,
+    status TEXT CHECK( status IN ('ToDo','InProgress','Done') ) NOT NULL DEFAULT 'ToDo',
+    description TEXT,
+    complexity INTEGER,
+    parent INTEGER,
+    created_date TIMESTAMP,
+    updated_date TIMESTAMP,
+    due_date TIMESTAMP,
+    priority TEXT CHECK( priority IN ('Low','Medium','High') ) NOT NULL DEFAULT 'Medium',
+);
+
+CREATE TABLE work_hour (
     issue TEXT NOT NULL,
     start_time TIMESTAMP,
-    end_time TIMESTAMP
+    end_time TIMESTAMP,
+
+    PRIMARY KEY(issue, start_time, end_time)
 );
 
 CREATE TABLE time_allocation (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
     project TEXT NOT NULL,
     period TIMESTAMP NOT NULL,
-    allocated_time INTEGER NOT NULL
+    allocated_time INTEGER NOT NULL,
+
+    PRIMARY KEY(project, period)
 );
 
 INSERT INTO task (name, project) VALUES ('GTMP-1', 'Project A');
