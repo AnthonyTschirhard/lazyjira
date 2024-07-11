dee8fe25518e76d0d5a9f14645995844299458ce
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-11 21:13:08 +0000

    singular form db table names

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,15 +1,15 @@
-CREATE TABLE tasks (
+CREATE TABLE task (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     project TEXT
 );
 
-CREATE TABLE projects (
+CREATE TABLE project (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
 
-CREATE TABLE work_hours (
+CREATE TABLE work_hour (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     issue TEXT NOT NULL,
     start_time TIMESTAMP,
@@ -23,15 +23,15 @@ CREATE TABLE time_allocation (
     allocated_time INTEGER NOT NULL
 );
 
-INSERT INTO tasks (name, project) VALUES ('GTMP-1', 'Project A');
-INSERT INTO tasks (name, project) VALUES ('GTMP-2', 'Project B');
-INSERT INTO tasks (name, project) VALUES ('GTMP-3', 'Project C');
+INSERT INTO task (name, project) VALUES ('GTMP-1', 'Project A');
+INSERT INTO task (name, project) VALUES ('GTMP-2', 'Project B');
+INSERT INTO task (name, project) VALUES ('GTMP-3', 'Project C');
 
-INSERT INTO work_hours (issue, start_time, end_time)
+INSERT INTO work_hour (issue, start_time, end_time)
 VALUES ('GTMP-1', '2024-07-01 09:00:00', '2024-07-01 17:00:00');
 
-INSERT INTO work_hours (issue, start_time, end_time)
+INSERT INTO work_hour (issue, start_time, end_time)
 VALUES ('GTMP-1', '2024-07-02 10:00:00', '2024-07-02 18:00:00');
 
-INSERT INTO work_hours (issue, start_time, end_time)
+INSERT INTO work_hour (issue, start_time, end_time)
 VALUES ('GTMP-2', '2024-07-03 08:00:00', '2024-07-03 16:00:00');
