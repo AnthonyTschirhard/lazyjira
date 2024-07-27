c22240c9c4a391c3ef32af9894a0aeb94b3799af
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-27 18:48:17 +0000

    add JIRA_STATUS_MAP in envs module

--- src/envs.py
+++ src/envs.py
@@ -4,6 +4,11 @@ JIRA_USER = os.environ['JIRA_USER']
 JIRA_TOKEN = os.environ['JIRA_API_TOKEN']
 JIRA_SERVER = "https://bic-americas.atlassian.net/"
 JIRA_FIELD_STORY_POINTS = "customfield_10005"
+JIRA_STATUS_MAP = {
+    "To Do": "TODO",
+    "In Progrss": "IN_PROGRESS",
+    "Done": "DONE",
+}
 GTM_OPS = 12401
 
 SQLITE_PATH = "/home/atschirhard/Sources/lazyjira/brother.db"
