bd2722ffe010240d167f6c658257f551855bd505
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-27 20:43:04 +0000

    update local task

--- src/conductor.py
+++ src/conductor.py
@@ -14,9 +14,6 @@ class Conductor():
         self.jira_client = jira_client
         self.db_client = db_client
 
-    def sync_jira_local(self):
-        pass
-
     def get_jira_issues(self):
         return [
             JiraTask(task)
@@ -29,6 +26,30 @@ class Conductor():
             for task in self.db_client.get_tasks()
         ]
 
+    def sync_jira_local(self):
+        jira_tasks = self.get_jira_issues()
+        db_tasks = self.get_db_issues()
+
+        jira_ids_mapping = {
+            task.jira_id: task
+            for task in db_tasks
+            if task.jira_id is not None
+        }
+
+        for task in jira_tasks:
+            if task.jira_id in jira_ids_mapping:
+                task.id = jira_ids_mapping[task.jira_id].id
+                self.update_local_task(task)
+            else:
+                # create a new task
+                pass
+
+    def update_local_task(
+        self,
+        jira_task: JiraTask,
+    ):
+        self.db_client.update_task(jira_task)
+
 
 if __name__ == "__main__":
     from envs import JIRA_USER, JIRA_TOKEN
@@ -37,5 +58,4 @@ if __name__ == "__main__":
     db_client = DBClient()
 
     conductor = Conductor(jira_client, db_client)
-    issues = conductor.get_db_issues()
-    issues = conductor.get_jira_issues()
+    conductor.sync_jira_local()
