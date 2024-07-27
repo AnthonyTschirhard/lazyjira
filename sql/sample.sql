c541ff7985a1347f44e8db50b15448c99ffb5b30
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-27 16:05:04 +0000

    create sample data

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,12 +1,22 @@
-INSERT INTO task (name, project) VALUES ('GTMP-1', 'Project A');
-INSERT INTO task (name, project) VALUES ('GTMP-2', 'Project B');
-INSERT INTO task (name, project) VALUES ('GTMP-3', 'Project C');
+INSERT INTO task (
+    summary,
+    jira_id,
+    description,
+    complexity
+) VALUES (
+    "Sample Summary",
+    "GTMP-6164",
+    "simple description",
+    10
+);
 
 INSERT INTO work_hour (issue, start_time, end_time)
-VALUES ('GTMP-1', '2024-07-01 09:00:00', '2024-07-01 17:00:00');
+VALUES ('GTMP-6164', '2024-07-01 09:00:00', '2024-07-01 17:00:00');
 
 INSERT INTO work_hour (issue, start_time, end_time)
-VALUES ('GTMP-1', '2024-07-02 10:00:00', '2024-07-02 18:00:00');
+VALUES ('GTMP-6164', '2024-07-02 10:00:00', '2024-07-02 18:00:00');
 
 INSERT INTO work_hour (issue, start_time, end_time)
-VALUES ('GTMP-2', '2024-07-03 08:00:00', '2024-07-03 16:00:00');
+VALUES ('GTMP-6164', '2024-07-03 08:00:00', '2024-07-03 16:00:00');
+
+ -- rm brother.db && sqlite3 brother.db < sql/ddl.sql && sqlite3 brother.db < sql/sample.sql
