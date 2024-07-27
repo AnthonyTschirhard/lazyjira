93517c4c9abd76c45bfb05d5d3803c417f5cd78e
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-27 20:43:04 +0000

    update local task

--- src/client/db_client.py
+++ src/client/db_client.py
@@ -1,4 +1,7 @@
-from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import select, insert, update
+
+from task.task import JiraTask
 
 from envs import SQLITE_PATH, TASK_TABLE
 
@@ -16,17 +19,6 @@ class DBClient():
             autoload_with=self.engine
         )
 
-    def create_task(
-        self,
-    ) -> None:
-        """creates an issue in the local database"""
-        pass
-
-    def update_task(
-        self,
-    ):
-        pass
-
     def get_tasks(self):
         stmt = select(self.task_table)
         with self.engine.connect() as con:
@@ -36,6 +28,17 @@ class DBClient():
             row._asdict()
             for row in rows
         ]
+
+    def update_task(
+        self,
+        task: JiraTask,
+    ) -> None:
+        update_stmt = update(self.task_table)
+
+        with self.engine.connect() as con:
+            record = task.to_record()
+            con.execute(update_stmt, task.to_record())
+            con.commit()
 
 
 if __name__ == "__main__":
