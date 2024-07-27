c0e2dac6e085b742a1d280e6511055d0f8414b75
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-27 17:30:18 +0000

    get_tasks in DBClient

--- src/db_client.py
+++ src/db_client.py
@@ -1,28 +1,42 @@
-from envs import SQLITE_PATH
+from sqlalchemy import create_engine, MetaData, Table, select
+
+from envs import SQLITE_PATH, TASK_TABLE
 
 
 class DBClient():
     """SQLite Client"""
 
-    def create_issue(
+    def __init__(self):
+        self.engine = create_engine(f"sqlite:////{SQLITE_PATH}")
+        self.metadata = MetaData()
+
+    def create_task(
         self,
-        project: int,
-        summary: str,
-        description: str,
-        issue_type: str,
-        assignee: str,
-        parent: str,
-        story_points: int = None,
-        version: str = None,
     ) -> None:
         """creates an issue in the local database"""
         pass
 
-    def update_issue(
+    def update_task(
         self,
     ):
         pass
 
+    def get_tasks(self):
+        task_table = Table(
+            TASK_TABLE,
+            self.metadata,
+            autoload_with=self.engine
+        )
+        stmt = select(task_table)
+        with self.engine.connect() as con:
+            rows = con.execute(stmt).all()
+
+        return [
+            row._asdict()
+            for row in rows
+        ]
+
 
 if __name__ == "__main__":
-    pass
+    client = DBClient()
+    client.get_tasks()
