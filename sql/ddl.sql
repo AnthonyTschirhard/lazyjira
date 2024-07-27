f65bf7794b1c9edbf9471d6385eb9f2f055dd659
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-07-27 15:56:22 +0000

    split ddl and sample sql script

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -33,16 +33,3 @@ CREATE TABLE time_allocation (
 
     PRIMARY KEY(project, period)
 );
-
-INSERT INTO task (name, project) VALUES ('GTMP-1', 'Project A');
-INSERT INTO task (name, project) VALUES ('GTMP-2', 'Project B');
-INSERT INTO task (name, project) VALUES ('GTMP-3', 'Project C');
-
-INSERT INTO work_hour (issue, start_time, end_time)
-VALUES ('GTMP-1', '2024-07-01 09:00:00', '2024-07-01 17:00:00');
-
-INSERT INTO work_hour (issue, start_time, end_time)
-VALUES ('GTMP-1', '2024-07-02 10:00:00', '2024-07-02 18:00:00');
-
-INSERT INTO work_hour (issue, start_time, end_time)
-VALUES ('GTMP-2', '2024-07-03 08:00:00', '2024-07-03 16:00:00');
